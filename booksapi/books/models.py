30ddefe39145e267069e4f8b80c27362ec02c9e4
deanastuart <deana.stuart16@gmail.com>
2020-12-21 13:54:05 -0500

    testing

--- booksapi/books/models.py
+++ booksapi/books/models.py
@@ -16,18 +16,18 @@ class Book(models.Model):
         return self.name
 
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# path = os.path.join(BASE_DIR, 'static/bestsellers-with-categories.csv')
-# with open(path) as f:
-#     reader = csv.reader(f)
-#     next(reader, None)
-#     for row in reader:
-#         _, created = Book.objects.get_or_create(
-#             name=row[0],
-#             author=row[1],
-#             rating=row[2],
-#             reviews=row[3],
-#             price=row[4],
-#             year=row[5],
-#             genre=row[6],
-#         )
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path = os.path.join(BASE_DIR, 'static/bestsellers-with-categories.csv')
+with open(path) as f:
+    reader = csv.reader(f)
+    next(reader, None)
+    for row in reader:
+        _, created = Book.objects.get_or_create(
+            name=row[0],
+            author=row[1],
+            rating=row[2],
+            reviews=row[3],
+            price=row[4],
+            year=row[5],
+            genre=row[6],
+        )
